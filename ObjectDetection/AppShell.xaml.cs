185f8ca72c9e197211d36b20d7d54e7ee6984635
Stelios Folias <s.folias@acmondata.gr>
2025-07-17 16:40:38 +0300

    test

--- ObjectDetection/AppShell.xaml.cs
+++ ObjectDetection/AppShell.xaml.cs
@@ -1,9 +1,6 @@
-﻿namespace ObjectDetection
-{
-    public partial class AppShell : Shell
-    {
-        public AppShell()
-        {
+﻿namespace ObjectDetection {
+    public partial class AppShell : Shell {
+        public AppShell() {
             InitializeComponent();
         }
     }
